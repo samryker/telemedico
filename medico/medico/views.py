4685ce472334dd5d15bde62aecd19faff132ec6e
AlliedMain <sam@gmail.com>
2023-01-30 02:59:19 +0530

    added /var/www/ in webhook

--- medico/medico/views.py
+++ medico/medico/views.py
@@ -4,7 +4,7 @@ import os
 
 def gitpull(request):
 
-    a=os.system("git -C /home/ubuntu/telemedico/ pull origin main")
+    a=os.system("git -C /var/www/telemedico/ pull origin main")
     a="hi"
-    os.system("touch /home/ubuntu/telemedico/medico/medico/wsgi.py")
+    os.system("touch /var/www/telemedico/medico/medico/wsgi.py")
     return HttpResponse(a, status=200)
