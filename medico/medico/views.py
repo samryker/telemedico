27efdc7fed35ea159a437700b43c40c55380a90f
AlliedMain <sam@gmail.com>
2023-01-30 03:06:39 +0530

    just added random text

--- medico/medico/views.py
+++ medico/medico/views.py
@@ -5,6 +5,6 @@ import os
 def gitpull(request):
 
     a=os.system("git -C /var/www/telemedico/ pull origin main")
-    a="hi"
+    a="Webhook working"
     os.system("touch /var/www/telemedico/medico/medico/wsgi.py")
     return HttpResponse(a, status=200)
